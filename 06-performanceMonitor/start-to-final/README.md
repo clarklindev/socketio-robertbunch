4521f44af66fd02188b48ff945d6f9429db8fbb8
clarklindev <clarklindev@gmail.com>
2024-03-23 10:57:12 +0800

    06-performanceMonitor-69-pulling-performance-data

--- 06-performanceMonitor/start-to-final/README.md
+++ 06-performanceMonitor/start-to-final/README.md
@@ -3,6 +3,18 @@ https://nodejs.org/docs/latest/api/os.html
 ## node
 https://github.com/socketio/socket.io-client
 
+### what do we need to know FROM NODE about performance?
+- CPU load (current)
+- memory usage
+    - total
+    - free
+- OS type
+- uptime
+- CPU info
+    - type
+    - no of cores
+    - clock speed
+
 ## server
 https://www.npmjs.com/package/socket.io
 https://www.npmjs.com/package/@socket.io/sticky
@@ -14,3 +26,4 @@ https://www.npmjs.com/package/@socket.io/cluster-adapter
 ### TROUBLESHOOTING
 - npm ERR! enoent ENOENT: no such file or directory, lstat 'C:\Users\lenovo\AppData\Roaming\npm'
 - FIX: create the folder... `mkdir %USERPROFILE%\AppData\Roaming\npm`
+
--- 06-performanceMonitor/start-to-final/node/index.js
+++ 06-performanceMonitor/start-to-final/node/index.js
@@ -1,3 +1,28 @@
 //node program that captures local performance data
 //and sends it via socket to the server
 //requires: socket.io-client
+const os = require('os');
+
+//what do we need to know FROM NODE about performance?
+
+    //cpu load (current)
+    const cpus = os.cpus(); //all cpus as an array
+    //memory usage
+    const usedMem = totalMem - freeMem;
+    const memUsage = Math.floor(usedMem / totalMem * 100) / 100;    //2 decimal places
+    //total
+    const totalMem = os.totalmem(); //bytes   
+    //free
+    const freeMem = os.freemem();//bytes
+    //OS type 
+    const osType = os.type();
+    //uptime
+    const upTime = os.uptime(); //uptime in seconds 
+    //CPU info
+    //type
+    const cpuType = cpus[0].model;
+    //no of cores
+    const numCores = cpus.length;
+    //clock speed
+    const cpuSpeed = cpus[0].speed;
+
